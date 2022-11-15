6b8e573ce913e1454093b7a1c339667f11555739
Martin Quintana <martinquintanarana@gmail.com>
2022-11-15 10:10:37 +0100

    15/11/22

---
+++ models/informacion.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import models, fields, api
+
+class informacion(models.Model):
+    _name = 'odoo_basico.informacion'
+    _description = 'Exemplo para infomacion'
+
+    name = fields.Char(string="Título:")
+    descripcion = fields.Text(string="A descripción:")
+    alto_en_cms = fields.Integer(string="Alto en Centímetros:")
+    ancho_en_cms = fields.Integer(string="Ancho en Centímetros:")
+    longo_en_cms = fields.Integer(string="Longo en Centímetros:")
